cbc27932ea73f992f5a50990ded829afdcd353d0
AzzuEsau <andres.lopez4191@alumnos.udg.mx>
2022-05-22 15:18:33 -0500

    first commit

---
+++ src/main/java/net/azzubanana/udemycourse/UdemyCourseMod.java
@@ -0,0 +1,20 @@
+package net.azzubanana.udemycourse;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// This is my main class
+public class UdemyCourseMod implements ModInitializer {
+	public static final String MOD_ID = "udemycourse";
+	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
